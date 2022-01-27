4976dafb797807da9cbd2dbe79ac68e446a05965
User52525252 <90308361+User52525252@users.noreply.github.com>
2022-01-27 15:21:10 +0100

    Django API
    An user data menagment API

---
+++ zadanie 4/tutorial/quickstart/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API pozwalające edytować dane użytkownika
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API pozwalające edytować dane użytkownika
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+from django.shortcuts import render
+
+# Create your views here.
